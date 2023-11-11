428105b275bd7e3768ebb5677850640eb14d5999
KamalLahloh <kamal.lahloh@gmail.com>
2023-11-12 00:04:52 +0300

    Project 1 - first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("2023-11-11");
+/* 
+Extra features:
+# Choose the time limit and show the timer.
+# Choose difficulty 2x2 3x3 4x4 ...
+# Show the pics for 10s or start the game immediately button.
+# Play music and stop button.
+# Reset button and shuffle again.
+# Hint button and give hint after 3 mistakes.
+# Dark theme.
+*/
