ce0e58796b7a536d2589a9b68ba4ad272beeca52
KamalLahloh <kamal.lahloh@gmail.com>
2023-11-12 05:02:51 +0300

    commit

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log("2023-11-11");
 /* 
 Extra features:
 # Choose the time limit and show the timer.
@@ -9,3 +8,56 @@ Extra features:
 # Hint button and give hint after 3 mistakes.
 # Dark theme.
 */
+
+const images = [
+    { id: 1, src: ""},
+    { id: 1, src: ""},
+    { id: 2, src: ""},
+    { id: 2, src: ""},
+    { id: 3, src: ""},
+    { id: 3, src: ""},
+    { id: 4, src: ""},
+    { id: 4, src: ""},
+    { id: 5, src: ""},
+    { id: 5, src: ""},
+    { id: 6, src: ""},
+    { id: 6, src: ""},
+    { id: 7, src: ""},
+    { id: 7, src: ""},
+    { id: 8, src: ""},
+    { id: 8, src: ""},
+    { id: 9, src: ""},
+    { id: 9, src: ""},
+    { id: 10, src: ""},
+    { id: 10, src: ""},
+    { id: 11, src: ""},
+    { id: 11, src: ""},
+    { id: 12, src: ""},
+    { id: 12, src: ""},
+  ];
+
+const body = document.querySelector("body");
+const header = document.querySelector("#header");
+const footer = document.querySelector("#footer");
+
+const mainWelcome = document.querySelector("#main-welcome");
+const music = document.querySelector("#music");
+const dark = document.querySelector("#dark");
+const logo = document.querySelector("#logo");
+const welcome = document.querySelector("#welcome");
+const number = document.querySelector("#number");
+const difficulty = document.querySelector("#difficulty");
+const time = document.querySelector("#time");
+const playNowButton = document.querySelector("#play-now-button");
+
+const mainIndex = document.querySelector("#main-index");
+const timer = document.querySelector("#timer");
+const motivation = document.querySelector("#motivation");
+const game = document.querySelector("#game");
+const hint = document.querySelector("#hint");
+const start = document.querySelector("#start");
+const resetButton = document.querySelector("#reset-button");
+
+
+
+
